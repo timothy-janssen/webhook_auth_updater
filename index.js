4b7100398e17264fcfe23e7afefa3a3530eca2fc
Timothy Janssen <timothy.janssen@sap.com>
2019-07-29 10:56:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ function add_template_to_webhooks() {
 
 					//await wait(1000)
 
-					return request.put(put_wh_credentials) 
-					//.delay(250)
+					return promise.put(put_wh_credentials) 
+					.delay(250)
 					.then(function (){
 						console.log("Added Auth to " + action.value.http_type + ": " + action.value.url)
 					})

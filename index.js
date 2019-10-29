8eb416bd8f05b3b6fa501ba9775c73b356cb591c
Timothy Janssen <timothy.janssen@sap.com>
2019-10-29 14:10:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ app.get('/where_used', function (req, res) {
         <!doctype html>
         <html>
         <body>
-            <form action="/where_used" method="post">
+            <form action="/where" method="post">
             	Bot Data<br>
                 Bot Owner ID: <input type="text" name="user_id" /><br>
                 Bot ID: <input type="text" name="bot_id" /><br>
